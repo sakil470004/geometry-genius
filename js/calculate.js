02512911885fd9e32eca6e5936a1815554c5222a
sakil470004 <sk470004@gmail.com>
2023-02-18 00:12:15 +0600

    finished display result function and fixed the bug

--- js/calculate.js
+++ js/calculate.js
@@ -3,23 +3,25 @@ function getInputFieldValueById(fieldId) {
     const inputValue = inputElement.value;
     return inputValue;
 }
-
-document.getElementById('calculate-triangle').addEventListener('click', function () {
-    const triangleValueB = getInputFieldValueById('triangle-b');
-    const triangleValueH = getInputFieldValueById('triangle-h');
-    const areaOfTriangle = 0.5 * triangleValueB * triangleValueH;
-    console.log(areaOfTriangle)
+// element create in this function and add to the area calculation Side
+function displayResult(elementName, area) {
     // area calculation container
     const areaCalculation = document.getElementById('area-calculation');
     const newLi = document.createElement('li');
     newLi.innerHTML =
         `
             <div class="flex justify-between items-center my-4">
-            <span>Triangle</span>
-            <span><span>${areaOfTriangle}</span> cm <sup>2</sup></span>
-            <button class="text-sm bg-sky-500 text-white font-bold px-8 py-2 rounded-lg">Convert to m
+            <span>${elementName}</span>
+            <span><span>${area}</span> cm <sup>2</sup></span>
+            <button class="text-sm bg-sky-500 text-white font-bold px-4 py-1 rounded-lg">Convert to m
                 <sup>2</sup></button>
             </div>
         `
-        areaCalculation.appendChild(newLi);
+    areaCalculation.appendChild(newLi);
+}
+document.getElementById('calculate-triangle').addEventListener('click', function () {
+    const triangleValueB = getInputFieldValueById('triangle-b');
+    const triangleValueH = getInputFieldValueById('triangle-h');
+    const areaOfTriangle = (0.5 * triangleValueB * triangleValueH).toFixed(2);
+    displayResult('Triangle', areaOfTriangle)
 })
