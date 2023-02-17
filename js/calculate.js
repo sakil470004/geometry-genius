3f30e5ac2d4569293be34f880f18f7552934b9d3
sakil470004 <sk470004@gmail.com>
2023-02-17 23:35:41 +0600

    finished ui design connect js

---
+++ js/calculate.js
@@ -0,0 +1,9 @@
+function getInputFieldValueById(fieldId) {
+    const inputElement = document.getElementById(fieldId)
+    const inputValue = inputElement.value;
+    return inputValue;
+}
+
+document.getElementById('calculate-triangle').addEventListener('click', function () {
+
+})
