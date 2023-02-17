da6817b20f78ccee00294fb1df3f900672a5d2f4
sakil470004 <sk470004@gmail.com>
2023-02-18 00:01:19 +0600

    finished triangle calculation

--- js/calculate.js
+++ js/calculate.js
@@ -5,5 +5,21 @@ function getInputFieldValueById(fieldId) {
 }
 
 document.getElementById('calculate-triangle').addEventListener('click', function () {
-
+    const triangleValueB = getInputFieldValueById('triangle-b');
+    const triangleValueH = getInputFieldValueById('triangle-h');
+    const areaOfTriangle = 0.5 * triangleValueB * triangleValueH;
+    console.log(areaOfTriangle)
+    // area calculation container
+    const areaCalculation = document.getElementById('area-calculation');
+    const newLi = document.createElement('li');
+    newLi.innerHTML =
+        `
+            <div class="flex justify-between items-center my-4">
+            <span>Triangle</span>
+            <span><span>${areaOfTriangle}</span> cm <sup>2</sup></span>
+            <button class="text-sm bg-sky-500 text-white font-bold px-8 py-2 rounded-lg">Convert to m
+                <sup>2</sup></button>
+            </div>
+        `
+        areaCalculation.appendChild(newLi);
 })
