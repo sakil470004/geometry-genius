1cf3f97c0de7cd63cff157c45e6f6931d3b2db61
sakil470004 <sk470004@gmail.com>
2023-02-18 11:27:52 +0600

    adding utility file for function

--- js/calculate.js
+++ js/calculate.js
@@ -1,34 +1,4 @@
-function getInputFieldValueById(fieldId) {
-    const inputElement = document.getElementById(fieldId)
-    const inputValue = parseFloat(inputElement.value);
-    inputElement.value = '';
-    return inputValue;
-}
-// element create in this function and add to the area calculation Side
-function displayResult(elementName, area) {
-    // area calculation container
-    const areaCalculation = document.getElementById('area-calculation');
-    const newLi = document.createElement('li');
-    newLi.innerHTML =
-        `<div class="flex justify-between gap-2 items-center my-4 text-sm">
-            <span>${elementName}</span>
-            <span><span>${area}</span> cm <sup>2</sup></span>
-            <button class="bg-sky-500 text-white font-bold px-4 py-1 rounded-lg">Convert to m
-                <sup>2</sup></button>
-        </div>`
-    areaCalculation.appendChild(newLi);
-}
-function isInputValid(number1, number2) {
-    if (number1 < 0 || number2 < 0) {
-        alert('Please give Valid input')
-        return false;
-    }
-    if (typeof number1 !== 'number' || typeof number2 !== 'number') {
-        alert('Please give Valid input')
-        return false;
-    }
-    return true;
-}
+
 // triangle
 document.getElementById('calculate-triangle').addEventListener('click', function () {
     const triangleValueB = getInputFieldValueById('triangle-b');
